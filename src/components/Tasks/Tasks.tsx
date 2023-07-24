7f4cc1f94fced40d509711ed937bafc60412fc02
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2023-07-24 18:39:15 +0300

    adding basic mark up for taskList

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,15 +1,17 @@
 import { Box, Button, Typography } from '@mui/material';
 import { TaskList } from './TaskList';
 import { useLocation } from 'react-router';
+import { toUpperFirstLetter } from '../../helpers/toUpperFirsLetter';
 
 export const Tasks = () => {
   const { state } = useLocation();
+  const categoryName = toUpperFirstLetter(state.category.name);
 
   return (
     <>
       <Box sx={{ display: 'flex' }}>
         <Typography component="span" sx={{ flexGrow: 1 }}>
-          {state?.category.name}
+          {categoryName}
         </Typography>
         <Button>Add task</Button>
       </Box>
