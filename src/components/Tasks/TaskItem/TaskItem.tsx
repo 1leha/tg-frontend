2e6b308845fee97f6e40a6e256c1e44825d935ce
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2023-07-25 10:07:54 +0300

    adding task edit feature

--- src/components/Tasks/TaskItem/TaskItem.tsx
+++ src/components/Tasks/TaskItem/TaskItem.tsx
@@ -18,7 +18,7 @@ export const TaskItem = ({ task }: IProps) => {
 
   return (
     <>
-      <Card sx={{ minWidth: 275, maxWidth: 325 }}>
+      <Card sx={{ minWidth: 275 }}>
         <CardContent>
           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
             {task.category?.name}
